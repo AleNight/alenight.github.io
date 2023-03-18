98856d9b8c0a78dfc7a8007c73841fb00d08ad82
AleNight <alejofermir@gmail.com>
2023-03-17 23:39:40 -0500

    17/03

---
+++ index.js
@@ -0,0 +1,31 @@
+let prevScrollpos = window.pageYOffset;
+let navbar = document.querySelector(".navbar");
+let logo = document.querySelector(".logo");
+let row = document.querySelector(".row");
+
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (currentScrollPos == 0) {
+    navbar.classList.remove("scroll-up");
+    navbar.classList.remove("scroll-down");
+    logo.classList.remove("scroll-up");
+    logo.classList.remove("scroll-down");
+    row.classList.remove("scroll-up");
+    row.classList.remove("scroll-down");
+  } else if (prevScrollpos < currentScrollPos && currentScrollPos > 1250) {
+    navbar.classList.add("scroll-up");
+    navbar.classList.add("scroll-down");
+    logo.classList.add("scroll-up");
+    logo.classList.add("scroll-down");
+    row.classList.add("scroll-up");
+    row.classList.add("scroll-down");
+  } else if (currentScrollPos > 125) {
+    navbar.classList.add("scroll-up");
+    navbar.classList.remove("scroll-down");
+    logo.classList.add("scroll-up");
+    logo.classList.remove("scroll-down");
+    row.classList.add("scroll-up");
+    row.classList.remove("scroll-down");
+  }
+  prevScrollpos = currentScrollPos;
+};
